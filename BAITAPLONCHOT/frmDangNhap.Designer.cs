59bedaa40c19128d5d3b3a82040bdf60d4df6b81
manhhungtmh <48795266+manhhungtmh@users.noreply.github.com>
2019-04-15 16:08:38 +0700

    Revert "tao da xoa button"

--- BAITAPLONCHOT/frmDangNhap.Designer.cs
+++ BAITAPLONCHOT/frmDangNhap.Designer.cs
@@ -36,11 +36,13 @@
             this.txtTenDangNhap = new System.Windows.Forms.TextBox();
             this.btnDangNhap = new System.Windows.Forms.Button();
             this.btnHuy = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
@@ -57,7 +59,7 @@
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkBox1.Location = new System.Drawing.Point(113, 83);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(103, 19);
+            this.checkBox1.Size = new System.Drawing.Size(123, 22);
             this.checkBox1.TabIndex = 2;
             this.checkBox1.Text = "Nhớ mật khẩu";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -70,7 +72,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(3, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.Size = new System.Drawing.Size(77, 18);
             this.label2.TabIndex = 3;
             this.label2.Text = "Mật khẩu :";
             // 
@@ -79,7 +81,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(3, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 15);
+            this.label1.Size = new System.Drawing.Size(113, 18);
             this.label1.TabIndex = 2;
             this.label1.Text = "Tên đăng nhập :";
             // 
@@ -88,7 +90,7 @@
             this.txtMatKhau.Location = new System.Drawing.Point(113, 45);
             this.txtMatKhau.Name = "txtMatKhau";
             this.txtMatKhau.PasswordChar = '*';
-            this.txtMatKhau.Size = new System.Drawing.Size(182, 21);
+            this.txtMatKhau.Size = new System.Drawing.Size(182, 24);
             this.txtMatKhau.TabIndex = 1;
             this.txtMatKhau.Text = "1";
             this.txtMatKhau.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMatKhau_KeyDown);
@@ -97,7 +99,7 @@
             // 
             this.txtTenDangNhap.Location = new System.Drawing.Point(113, 3);
             this.txtTenDangNhap.Name = "txtTenDangNhap";
-            this.txtTenDangNhap.Size = new System.Drawing.Size(182, 21);
+            this.txtTenDangNhap.Size = new System.Drawing.Size(182, 24);
             this.txtTenDangNhap.TabIndex = 0;
             this.txtTenDangNhap.Text = "NV00000011";
             this.txtTenDangNhap.TextChanged += new System.EventHandler(this.txtTenDangNhap_TextChanged);
@@ -123,9 +125,18 @@
             this.btnHuy.UseVisualStyleBackColor = true;
             this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmDangNhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(323, 167);
@@ -155,6 +166,7 @@
         private System.Windows.Forms.Button btnDangNhap;
         private System.Windows.Forms.Button btnHuy;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
